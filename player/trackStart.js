9f2ea1c752a4ce6e82b305e37b2070edc25fa7ae
Killerpac <nilavasarkar@gmail.com>
2021-08-10 13:12:18 +0530

    make it better

--- player/trackStart.js
+++ player/trackStart.js
@@ -1,5 +1,3 @@
 module.exports = (client, queue, track) => {
-    queue.metadata.send({embeds:[{title:`🎶 | Now Playing`,description:`[${track.title}](${track.url}) in **${queue.connection.channel.name}**!`,color:`${client.colour}`}]}).then(async mes => {
-        await client.config.discord.ne.push(mes);
-    });
+    queue.metadata.send({embeds:[{title:`🎶 | Now Playing`,description:`[${track.title}](${track.url}) in **${queue.connection.channel.name}**!`,color:`${client.colour}`}]})
 }
