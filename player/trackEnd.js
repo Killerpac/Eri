123c71126ddf8dfd473d57edd5c68bc3a9fc2c0c
Killerpac <nilavasarkar@gmail.com>
2021-08-10 11:46:39 +0530

    make it beter

--- player/trackEnd.js
+++ player/trackEnd.js
@@ -1,4 +1,13 @@
 module.exports = async (client,queue,track) => {
-    const x =client.config.discord.ne.find(m => m.guild == queue.metadata.guild)
-    await x.delete();
+    let array = client.config.discord.ne
+    function remove(arr, value) {
+        var index = arr.indexOf(value);
+        if (index > -1) {
+          arr.splice(index, 1);
+        }
+        return arr;
+      }
+    const x = array.find(m => m.guild == queue.metadata.guild)
+    await x.delete()
+    client.config.discord.ne =  remove(array,x)
 }
