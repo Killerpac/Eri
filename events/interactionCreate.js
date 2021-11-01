7ed7c3d26a33345df5d8259e5e416508b47b8bd8
killerpac <nilavasarkar.cse.jisu@gmail.com>
2021-11-01 17:50:40 +0530

    make the buttons

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,43 @@
-module.exports = (client,interaction) =>
+const skip = require("../commands/music/skip")
+const pause = require("../commands/music/pause")
+const resume = require("../commands/music/resume")
+const stop = require("../commands/music/stop")
+const queue = require("../commands/music/queue")
+
+module.exports = async (client,interaction) =>
 {
+    //buttom names pause/resumebtn,skipbtn,stopbtn,queuebtn
+    if(interaction.isButton()) {
+        if(interaction.customId == "pause/resumebtn")
+        {
+            const queue = client.player.getQueue(interaction.guildId);
+            if(queue.paused) {
+                resume.execute(client,interaction);
+            }
+            else {
+                pause.execute(client,interaction);
+            }
+        
+        }
+        else if(interaction.customId == "skipbtn")
+        {
+            skip.execute(client,interaction);
+        }
+        else if(interaction.customId == "stopbtn")
+        {
+            stop.execute(client,interaction);
+        }
+        else if(interaction.customId == "queuebtn")
+        {
+            queue.execute(client,interaction);
+        }  
+    }
     if (!interaction.isCommand() || !interaction.guildId) return;
 
 	if (!client.commands.has(interaction.commandName)) return;
 
-	const cmd = client.commands.get(interaction.commandName)
+	const cmd = client.commands.get(interaction.commandName);
 
     if(cmd) cmd.execute(client, interaction);
+
 }
--- player/playSong.js
+++ player/playSong.js
@@ -1,5 +1,25 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
 module.exports = (client, queue, song) => {
-    queue.textChannel.send({embeds:[{title:`🎶 | Now Playing`,description:`[${song.name}](${song.url}) in **${queue.voiceChannel.name}**!`,color:`${client.colour}`}]}).then(mes => {
+    const row = new MessageActionRow()
+    .addComponents(
+        new MessageButton()
+            .setCustomId('pause/resumebtn')
+            .setLabel('Pause/Resume')
+            .setStyle('SUCCESS'),
+        new MessageButton()
+            .setCustomId('skipbtn')
+            .setLabel('Skip')
+            .setStyle('SECONDARY'),
+        new MessageButton()
+            .setCustomId('stopbtn')
+            .setLabel('Stop')
+            .setStyle('DANGER'),
+        new MessageButton()
+            .setCustomId('queuebtn')
+            .setLabel('Show Queue')
+            .setStyle('PRIMARY')
+    );
+    queue.textChannel.send({embeds:[{title:`🎶 | Now Playing`,description:`[${song.name}](${song.url}) in **${queue.voiceChannel.name}**!`,color:`${client.colour}`}],components: [row]}).then(mes => {
         client.config.discord.ne.push(mes);
     })
 }
