b13b014c198be7a8e0e5a9969907a9859cd2d1cf
killerpac <nilavasarkar.cse.jisu@gmail.com>
2021-11-03 15:58:05 +0530

    fix message update

--- player/finishSong.js
+++ player/finishSong.js
@@ -1,5 +1,5 @@
 module.exports = (client, queue) => {
-    let x = client.config.discord.ne.find(e => e.guildId == queue.textChannel.guild.id);
+    let x = client.config.discord.ne.find(e => e.guildId == queue.textChannel.guildId);
     if (x) { 
         x.delete()
         let y = client.config.discord.ne.indexOf(x)
