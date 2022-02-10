ce02432fca720bcf20cb99b2a67c9a31f9ccec34
Killerpac <nilavasarkar@gmail.com>
2022-02-10 22:35:56 +0530

    update deps

--- player/error.js
+++ player/error.js
@@ -1,6 +1,7 @@
 module.exports = (client ,queue, error) => {
     console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
     try{
+        queue.textChannel.send({embeds:[{description:`❌ | I Crashed HELP!!!!!`,color:`${client.colour}`}]});
         let x = client.config.discord.ne.find(e => e.guildId == queue.textChannel.guild.id);
         if (x) { 
             x.delete()
